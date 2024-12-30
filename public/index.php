1af45eac22db1151d7c5d966ded5c2ba45bca6af
student3B032081 <3b032081@gm.student.ncut.edu.tw>
2024-12-30 21:45:16 +0800

    auto類別自動載入:範例1_23頁

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+require '../vendor/autoload.php';
+
+//類別先import進來
+use Demo\Hello\Lara;
+use Demo\Hello\Someone;
+$lara= new Lara();
+$vincent= new Someone('Vincent');
+
+//類別未先import進來
+$mary= new \Demo\Hello\Someone('Mary');
+$john= new Demo\Hello\Someone('John');
+$hello= new Demo\HelloWorld();
+
+use Demo\HelloWorld as World; //類別另取別名
+$world = new World();
