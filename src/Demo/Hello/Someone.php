761e2178a086834868bb695b5a877b56872bdca4
student3B032081 <3b032081@gm.student.ncut.edu.tw>
2024-12-30 21:42:14 +0800

    auto類別自動載入:範例1_22頁

---
+++ src/Demo/Hello/Someone.php
@@ -0,0 +1,10 @@
+<?php
+namespace Demo\Hello;
+
+class Someone
+{
+    function __construct($name)
+    {
+        echo 'Hello '.$name.'~<br>'.PHP_EOL;
+    }
+}
---
+++ src/Demo/HelloWorld.php
@@ -0,0 +1,10 @@
+<?php
+namespace Demo;
+
+class HelloWorld
+{
+    function __construct()
+    {
+        echo 'Hello World~<br>'.PHP_EOL;
+    }
+}
